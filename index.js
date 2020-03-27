495d1dd215c3e1fa63ee66e5bbd55f7190925912
jamg <jammmg26@gmail.com>
2020-03-27 13:23:10 +0800

    Initial JS and Mongo Setup with cors and bodyparser

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+//Models
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// Database Setup
+mongoose.connect(
+  'mongodb+srv://admin:admin@cluster0-kgze8.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// Server Setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('listening in port ' + PORT);
+});
