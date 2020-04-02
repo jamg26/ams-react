477e9078d3bf276ffb9bf25a0ef7ec4ca3bcc4da
jamg <jammmg26@gmail.com>
2020-04-02 16:06:40 +0800

    Update index.js

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = props => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Go to <Link to='/bulletin'>bulletin</Link>
+    </div>
+  );
 };
 
 export default Home;
