547360baecafbc81ce059271a802958b8dad93d1
jamg <jammmg26@gmail.com>
2020-04-08 15:03:01 +0800

    Transaction UI, Update Routing(Replace Placeholder Page)
    * Added Checkout forms

--- client/src/components/pages/asset/transaction/index.js
+++ client/src/components/pages/asset/transaction/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import CheckOut from './components/check-out';
 
-const Transaction = props => {
-  return <div>Asset Transaction</div>;
+const Transaction = (props) => {
+  const { TabPane } = Tabs;
+  return (
+    <div>
+      <Tabs defaultActiveKey='checkOut' size='large'>
+        <TabPane tab='Check Out' key='checkOut'>
+          <CheckOut />
+        </TabPane>
+        <TabPane tab='Check In' key='checkIn'>
+          Check In
+        </TabPane>
+        <TabPane tab='Move/Assign To' key='moveAssignTo'>
+          Move/Assign To
+        </TabPane>
+        <TabPane tab='Dispose' key='dispose'>
+          Dispose
+        </TabPane>
+        <TabPane tab='Recover' key='recover'>
+          Recover
+        </TabPane>
+        <TabPane tab='Maintenance' key='maintenance'>
+          Maintenance
+        </TabPane>
+        <TabPane tab='Extend Check Out' key='extendCheckOut'>
+          Extend Checkout
+        </TabPane>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Transaction;
