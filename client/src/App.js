aa48f4356daeb1aa47ccf9fdb02b5025d587ec51
Jamuel Galicia <jammmg26@gmail.com>
2020-04-08 17:52:29 +0800

    re

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Routes from './routes';
-import Home from './components/pages/home';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Routes from "./routes";
+import Home from "./components/pages/home";
 
-const App = props => {
+const App = (props) => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path="/" component={Home} />
           <Routes />
         </Switch>
       </BrowserRouter>
